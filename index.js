33748859038cee79d360ec1cd589e54f068e2359
alami <alami@km.ru>
2021-04-27 19:19:48 +0500

    add N-N "photos-tagged_user" branches

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const typeDefs = `
         description: String
         category: PhotoCategory!
         postedBy: User!
+        taggedUsers: [User!]!
     }
     input PostPhotoInput {
         name: String!
@@ -26,6 +27,7 @@ const typeDefs = `
         name: String
         avatar: String
         postedPhotos: [Photo!]!
+        inPhotos: [Photo!]!
     }
     type Query {
         totalPhotos: Int!
@@ -65,6 +67,12 @@ var photos = [    // var photos = []
         "githubUser": "sSchmidt"
     }
 ]
+var tags = [
+    { "photoID": "1", "userID": "gPlake" },
+    { "photoID": "2", "userID": "sSchmidt" },
+    { "photoID": "2", "userID": "mHattrup" },
+    { "photoID": "2", "userID": "gPlake" }
+]
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length,
@@ -92,12 +100,20 @@ const resolvers = {
         url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
         postedBy: parent => {
             return users.find(u => u.githubLogin === parent.githubUser)
-        }
+        },
+        taggedUsers: parent => tags
+            .filter(tag => tag.photoID === parent.id)
+            .map(tag => tag.userID)
+            .map(userID => users.find(u => u.githubLogin === userID))
     },
     User: {
         postedPhotos: parent => {
             return photos.filter(p => p.githubUser === parent.githubLogin)
-        }
+        },
+        inPhotos: parent => tags
+            .filter(tag => tag.userID === parent.id)
+            .map(tag => tag.photoID)
+            .map(photoID => photos.find(p => p.id === photoID))
     }
 }
 const server = new ApolloServer({
