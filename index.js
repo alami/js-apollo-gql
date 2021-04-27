ccbbbad970a62c00d76f4295edc89dd97cfee865
alami <alami@km.ru>
2021-04-27 13:35:31 +0500

    add type Photo and query&mutation

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 // 1. Требуется 'apollo-server'.
 const { ApolloServer } = require('apollo-server')
 const typeDefs = `
+    # 1. Добавляем определение типа Photo.
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+    }
     type Query {
         totalPhotos: Int!
+        allPhotos: [Photo!]! # 2. Возвращаем Photo
     }
+    # 3. Возвращаем недавно опубликованную фотографию из мутации.
     type Mutation {
-        postPhoto(name: String! description: String): Boolean!
+        postPhoto(name: String! description: String): Photo!
     }
 `
+// 1. Переменная, которую мы будем увеличивать для уникальных идентификаторов.
+var _id = 0
 // 1. Тип данных для хранения ваших фотографий в памяти.
 var photos = []
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length, // 2. Возвращаем длину массива фотографий.
+        allPhotos: () => photos
     },
     // 3. Распознаватель Mutation и postPhoto.
     Mutation: {
         postPhoto(parent, args) {
-            photos.push(args)
-            return true
+            // 2. Создаем новую фотографию и генерируем идентификатор.
+            var newPhoto = {
+                id: _id++,
+                ...args
+            }
+            photos.push(newPhoto)
+            return newPhoto  // 3. Возвращаем новую фотографию.
         }
     }
 }
