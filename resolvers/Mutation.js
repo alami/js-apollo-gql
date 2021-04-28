e7b5c91a22443904da09c18ba31801bbfe566f1a
alami <alami@km.ru>
2021-04-28 15:30:32 +0500

    devide on resolvers/modules v.3

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,20 @@
+var {_id, users, photos, tags} = require('./Vars');
 module.exports = {
-
+    postPhoto(parent, args) {
+        var newPhoto = {
+            id: _id++,
+            ...args
+        }
+        photos.push(newPhoto)
+        return newPhoto
+    },
+    postPhoto1(parent, args) {
+        var newPhoto = {
+            id: _id++,
+            ...args.input,
+            created: new Date()
+        }
+        photos.push(newPhoto)
+        return newPhoto
+    }
 }
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,3 +1,9 @@
+var {_id, users, photos, tags} = require('./Vars');
 module.exports = {
-
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+    allPhotosD: (parent, args) => {
+        args.after
+        photos
+    }
 }
